e6b153bdf59bc2d3f7671c9bd4a8544a4f1a049a
MarcelKubicki <s091284@student.tu.kielce.pl>
2024-03-11 10:51:53 +0100

    first classes

---
+++ src/main/java/com/example/isi_project_backend/IsiProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.isi_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsiProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsiProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/isi_project_backend/IsiProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.isi_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsiProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
